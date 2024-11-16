a115d5529f6edcb1cece365af10526e6421af462
Paul Moss <paulmoss06@gmail.com>
2024-11-16 10:37:50 +0000

    build: another commit

--- app.config.mjs
+++ app.config.mjs
@@ -3,6 +3,10 @@ import { createApp } from "vinxi";
 import { extendTheme, pigment } from '@stylefusion/vite-plugin';
 import { getPigmentCSSTheme } from '@raikou/system';
 import { config } from "vinxi/plugins/config";
+import { dirname } from "node:path";
+
+// get path
+const __dirname = dirname(new URL(import.meta.url).pathname);
 
 const { cssTheme, rawTheme } = getPigmentCSSTheme();
 
@@ -25,15 +29,17 @@ export default createApp({
           atomic: false,
           theme,
           rawTheme,
-          transformLibraries: ['@raikou/core', '@raikou/system'],
+          transformLibraries: ['@raikou/core', '@raikou/system', '@raikou/emotion'],
         }),
         reactRefresh(),
         config("custom", {
           resolve: {        
             alias: {
-              'prop-types': 'prop-types/prop-types.js',
-              'react-is': 'react-is/cjs/react-is.development.js',
-              'flush-sync': 'flush-sync/dist/flush-sync.js',
+              'prop-types': `${__dirname}/node_modules/prop-types/prop-types.js`,
+              'react-is': `${__dirname}/node_modules/react-is/cjs/react-is.development.js`,
+              'flush-sync': `${__dirname}/node_modules/flush-sync/dist/flush-sync.js`,
+              'hoist-non-react-statics': `${__dirname}/node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs`,
+              'html-react-parser': `${__dirname}/node_modules/html-react-parser/dist/html-react-parser.js`,
             }  
           }          
         }),
---
+++ app.config.timestamp_1731753108501.mjs
@@ -0,0 +1,46 @@
+// app.config.mjs
+import reactRefresh from "@vitejs/plugin-react";
+import { createApp } from "vinxi";
+import { extendTheme, pigment } from "@stylefusion/vite-plugin";
+import { getPigmentCSSTheme } from "@raikou/system";
+import { config } from "vinxi/plugins/config";
+var { cssTheme, rawTheme } = getPigmentCSSTheme();
+var theme = extendTheme({
+  cssVarPrefix: "raikou",
+  getSelector: (colorScheme) => colorScheme ? `[data-raikou-color-scheme='${colorScheme}']` : ":root",
+  ...cssTheme
+});
+console.log(__dirname);
+var app_config_default = createApp({
+  routers: [
+    {
+      name: "client",
+      type: "spa",
+      handler: "./index.html",
+      target: "browser",
+      plugins: () => [
+        pigment({
+          atomic: false,
+          theme,
+          rawTheme,
+          transformLibraries: ["@raikou/core", "@raikou/system", "@raikou/emotion"]
+        }),
+        reactRefresh(),
+        config("custom", {
+          resolve: {
+            alias: {
+              "prop-types": "prop-types/prop-types.js",
+              "react-is": "react-is/cjs/react-is.development.js",
+              "flush-sync": "flush-sync/dist/flush-sync.js",
+              "hoist-non-react-statics": "hoist-non-react-statics/dist/hoist-non-react-statics.cjs",
+              "html-react-parser": "/Users/paul/development/src/github/tanstack/vinxi/node_modules/html-react-parser/dist/html-react-parser.js"
+            }
+          }
+        })
+      ]
+    }
+  ]
+});
+export {
+  app_config_default as default
+};
